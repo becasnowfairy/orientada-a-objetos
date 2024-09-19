9ac6b368eeb4d5bc47fffeb0f341767f18d99fcf
becas <becasnowfairy@gmail.com>
2024-09-19 14:37:47 +0100

    help me

--- EXERCICIOS.java
+++ EXERCICIOS.java
@@ -2,7 +2,7 @@ package Sessao2;
 
 import java.util.Scanner;
 
-public class Exercicio {
+public class EXERCICIOS {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
